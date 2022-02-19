c590b1ead54e8881d2baad295376d723dd05dc06
Ehsan EbneKhodadad <you@ehsan.ebnekhodadad@gmail.com>
2022-02-19 16:51:32 +0330

    add getStaticProps to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,37 @@
 import { Fragment } from "react";
 import MeetupItems from "../components/meetup/meetupItems/meetupItems";
 
-function HomePage() {
-  const DUMMY_DATA = [
-    {
-      id: "p1",
-      title: "First Title",
-      image: "image1.jpg",
-      address: "some address",
-    },
-    {
-      id: "p2",
-      title: "First Title",
-      image: "image1.jpg",
-      address: "some address",
-    },
-    {
-      id: "p3",
-      title: "First Title",
-      image: "image1.jpg",
-      address: "some address",
-    },
-  ];
-
+function HomePage(props) {
   return (
     <Fragment>
-      <MeetupItems data={DUMMY_DATA} />
+      <MeetupItems data={props.data} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const dataJson = await fetch(
+    "https://meetups-58b30-default-rtdb.firebaseio.com/meetups.json"
+  );
+  const data = await dataJson.json();
+  let array = [];
+
+  for (let item in data) {
+    array.push({
+      id: item,
+      title: data[item].title,
+      image: data[item].image,
+      address: data[item].address,
+      description: data[item].description,
+    });
+  }
+
+  return {
+    props: {
+      data: array,
+    },
+    revalidate: 1,
+  };
 }
 
 export default HomePage;
